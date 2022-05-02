ab39ae863b29a9364e667650476299cea84c29fc
newface95@icloud.com <newface95@icloud.com>
2022-05-02 16:21:16 +0900

    Updates

---
+++ precache-manifest.97405ae7584a56116c2ffb04f08354e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758f0d69dc3dd5bbb861ed852d76a8bd",
+    "url": "/Unichat/index.html"
+  },
+  {
+    "revision": "efccb670670cffcbfe5e",
+    "url": "/Unichat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "7b8539d6fe478ba15d22",
+    "url": "/Unichat/static/js/2.bab4f2c5.chunk.js"
+  },
+  {
+    "revision": "e4a149e709df605ab7285675e59c272a",
+    "url": "/Unichat/static/js/2.bab4f2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efccb670670cffcbfe5e",
+    "url": "/Unichat/static/js/main.9b7539f5.chunk.js"
+  },
+  {
+    "revision": "02956b7f5d65ea9c150e",
+    "url": "/Unichat/static/js/runtime-main.94af1af1.js"
+  }
+]);
